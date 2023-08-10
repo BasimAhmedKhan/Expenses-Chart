c2e2cbb29ccf6ec8378e38a8b02566ebf495ae4f
Basim Ahmed Khan <kbasim039@gmail.com>
2023-08-10 20:33:48 +0500

    fix: Day update bug.

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,16 +1,10 @@
 import styles from './Column.module.css'
 
 export default function Column(props){
-    let weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    let date = new Date();
-    let day = date.getDay();
-    let currentDay = "";
-    let str = weekDays[day].slice(0, 3).toLocaleLowerCase();
-    if(str == props.day){currentDay = styles.currentDay;}
     return(
         <div className={`center ${styles.chartContainer}`}>
             <div className={`center ${styles.amount}`}>${props.amount}</div>
-            <div className={`${styles.column} ${currentDay}`} style={{height: `${props.amount*2.5}px`}}></div>
+            <div className={`${styles.column} ${props.day==(new Date()).toString().split(' ')[0].toLocaleLowerCase() ? styles.currentDay : ""}`} style={{height: `${props.amount*2.5}px`}}></div>
             <p style={{color: 'var(--MediumBrown)'}}>{props.day}</p>
         </div>
     )
